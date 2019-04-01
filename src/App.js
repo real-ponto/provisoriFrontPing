5f1b2aa71bf082c11f0179765ee3c5bbf4f6c2ad
Guilherme Stain <guilhermestain@gmail.com>
2019-04-01 16:38:26 -0300

    front: reset modulo

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import { message, Modal } from 'antd';
 import "antd/dist/antd.css";
 import resetRelogio from './service/reset'
 import getStatus from './service/getStatus'
-import { Spin } from 'antd';
+import { Spin } from 'antd'
 import { Progress } from 'antd';
 
-
-
 const defaultState = {
-  IP:'',
+  IP: '',
   data: {
-    tempoLigado : '0', 
-    tempoConectado : '0',
-    tempoDesconectado : '0',
-    quantVezesPerdeuConexao : '0',
-    ping:'0' 
+    tempoLigado: '0',
+    tempoConectado: '0',
+    tempoDesconectado: '0',
+    quantVezesPerdeuConexao: '0',
+    ping: '0',
+    firmwareVersion: '0',
+    percent: '0'
   },
   visible: false,
   loading: false
 }
-
 
 class App extends Component {
 
@@ -49,27 +48,35 @@ class App extends Component {
 
   showModal = () => {
     this.setState({
-      visible:true,
-      loading: true
+      visible: true
     })
   }
 
   getStatus = async () => {
     const IP = this.state.IP
-    console.log(this.state.IP)
-    try {
-      const { data } = await getStatus(IP)
+    this.setState({
+      loading: true
+    })
+    const { data, success } = await getStatus(IP)
+    if (success) {
       this.setState({
+        loading: false,
         data: data
       }, this.showModal)
-    } catch (error) {
+    } else {
+      this.setState({
+        loading: false,
+        data: defaultState.data
+      })
       message.error('Relógio não encontrado, tente novamente', 10);
     }
   }
 
-
   resetRelogio = async () => {
     const IP = this.state.IP
+    this.setState({
+      loading: true
+    })
     const success = await resetRelogio(IP)
 
     const deucerto = () => {
@@ -78,46 +85,61 @@ class App extends Component {
     const failed = () => {
       message.error('Relógio não foi resetado', 10);
     }
-    if (success) deucerto()
-    else failed()
-
-
+    if (success) {
+      this.setState({
+        loading: false
+      }, deucerto())
+    } else {
+      this.setState({
+        loading: false,
+      }, failed())
+    }
   }
 
   render() {
     const { data } = this.state
-    const { 
-      tempoLigado = '0', 
+    const {
+      tempoLigado = '0',
       tempoConectado = '0',
       tempoDesconectado = '0',
-      quantasVezesTPLinkReiniciou = '0' ,
-      ping = '0'
+      quantasVezesTPLinkReiniciou = '0',
+      ping = '0',
+      firmwareVersion = '0',
+      percent = '0'
     } = data
-    const formattedPercent = Math.floor((tempoConectado/tempoLigado)*100)
     return (
-      <div className="App">
-        <div className='div-info'>
-          <Input changed={this.setValueToInput} />
-          <Button1 name='Reset' click={this.resetRelogio} changed={this.setValueToInput} />
-          <Button1 name='Status' click={this.getStatus} changed={this.setValueToInput}/>
-          <Modal
-            title="Status do módulo"
-            visible={this.state.visible}
-            onOk={this.handleOk}
-            onCancel={this.handleCancel}
-          >
-          <div className='grafico'>
-            <h3>Qualidade:</h3>
-            <Progress type="circle" percent={formattedPercent} strokeColor='yellowgreen'  />
+      <div className='main'>
+        <div className="App">
+          <div className='div-info'>
+            <Input changed={this.setValueToInput} />
+            <Button1 name='Reset' click={this.resetRelogio} changed={this.setValueToInput} />
+            <Button1 name='Status' click={this.getStatus} changed={this.setValueToInput} />
+            <div className='div-spin'>
+              <Spin
+                spinning={this.state.loading}
+                size='large'
+              />
+            </div>
+            <Modal
+              title="Status do módulo"
+              visible={this.state.visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              <div className='grafico'>
+                <h3>Qualidade:</h3>
+                <Progress type="circle" percent={percent} strokeColor='yellowgreen' />
+              </div>
+              <div className='div-modal'>
+                <div className='div-p'><p className='paragrafoLigado'> Tempo Ligado:</p>            {tempoLigado} </div>
+                <div className='div-p'><p className='paragrafoResposta'> Tempo de resposta:</p>     {ping} ms </div>
+                <div className='div-p'><p className='paragrafoConectado'> Tempo Conectado:</p>      {tempoConectado} </div>
+                <div className='div-p'><p className='paragrafoDesc'> Tempo Desconectado:</p>        {tempoDesconectado} </div>
+                <div className='div-p'><p className='paragrafoTP'> TP - Link reset:</p>             {quantasVezesTPLinkReiniciou} vezes </div>
+                <div className='div-p'><p className='paragrafoFirmware'> Versão do Firmware:</p>      {firmwareVersion} </div>
+              </div>
+            </Modal>
           </div>
-          <div className='div-modal'>
-          <div className='div-p'><p className='paragrafoLigado'> Tempo Ligado:</p>            {tempoLigado} min</div>
-          <div className='div-p'><p className='paragrafoResposta'> Tempo de resposta:</p>     {ping} ms </div>
-          <div className='div-p'><p className='paragrafoConectado'> Tempo Conectado:</p>      {tempoConectado} min </div>
-          <div className='div-p'><p className='paragrafoDesc'> Tempo Desconectado:</p>        {tempoDesconectado} min </div>
-          <div className='div-p'><p className='paragrafoTP'> TP - Link reset:</p>             {quantasVezesTPLinkReiniciou} vezes </div>
-          </div>
-          </Modal>
         </div>
       </div>
     );
--- src/service/getStatus.js
+++ src/service/getStatus.js
@@ -2,26 +2,37 @@ import axios from  'axios'
 
 const getStatus = async IP =>{
   
+  const formatMinutes = (min) =>{
+    min = parseInt(min, 10)
+    let dias = Math.floor(min/1440)
+    min = min - (dias*1440)
+    let horas = Math.floor(min/60)
+    min = min - (horas*60)
+
+    return `${dias} dias ${horas}h ${min}min`
+  }
+
   let responseObject = { 
     success: false 
   }
-
-  await axios.get(`http://${IP}:4000/getStatus`)
+  
+  await axios.get(`http://${IP}:4000/getStatus`, { timeout:20000} )
   .then(function (response) {
     if(response.status === 200){
       responseObject.success = true
 
       const dataFormatted = (response.data).split(';')
-      console.log(dataFormatted)
       responseObject = {
         ...responseObject,
         data:{
-          tempoLigado: dataFormatted[0], 
-          tempoConectado: dataFormatted[1],
-          tempoDesconectado: dataFormatted[0] - dataFormatted[1] ,
+          tempoLigado: formatMinutes(dataFormatted[0]), 
+          tempoConectado: formatMinutes(dataFormatted[1]),
+          tempoDesconectado: formatMinutes(dataFormatted[0] - dataFormatted[1]),
           quantasVezesTPLinkReiniciou: dataFormatted[2],
-          ping: dataFormatted[3]
-        } 
+          ping: dataFormatted[3],
+          firmwareVersion: dataFormatted[4],
+          percent: Math.floor((dataFormatted[1]/dataFormatted[0])*100)
+        }
       }
     }
   })
